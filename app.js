c13b4104d6ec6d98b632b9036bfff505bc48c548
RicardoNeto94 <49795011+RicardoNeto94@users.noreply.github.com>
2025-10-13 11:45:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const allergenClassMap = {
 };
 
 const LEGEND = {
-  CE:"Celery", GL:"Gluten", CR:"Crustaceans", EG:"Eggs", FI:"Fish", MO:"Molluscs", Mi:"Milk",
+  CE:"Celery", GL:"Gluten", CR:"Crustaceans", EG:"Eggs", FI:"Fish", MO:"Molluscs", MI:"Milk",
   MU:"Mustard", NU:"Nuts", SE:"Sesame", SO:"Soya", GA:"Garlic", ON:"Onion", MR:"Mushrooms", HO:"Honey"
 };
 
